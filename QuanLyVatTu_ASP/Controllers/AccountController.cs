840ea55c3e0e8546966202b87bde17aa1ee990e0
dainh0607 <nguyenhoangdai67d@gmail.com>
2025-11-24 15:14:47 +0700

    Hoàn thành giao diện đăng nhập

---
+++ QuanLyVatTu_ASP/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using QuanLyVatTu_ASP.Models;
+
+namespace QuanLyVatTu_ASP.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Auth()
+        {
+            return View();
+        }
+
+    }
+}
